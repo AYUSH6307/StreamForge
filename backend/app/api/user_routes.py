9a59be52a44f911666ba049c2f82768da8e1f081
AYUSH6307 <2k22.cse.2213378@gmail.com>
2026-07-19 19:49:44 +0530

    Implemented user registration and login APIs

--- backend/app/api/user_routes.py
+++ backend/app/api/user_routes.py
@@ -5,9 +5,29 @@ from app.core.database import get_db
 from app.schemas.user import UserCreate, UserResponse
 from app.services.user_service import create_user
 
+from fastapi import HTTPException
+from app.schemas.user import UserLogin
+from app.services.user_service import authenticate_user
+
 router = APIRouter(prefix="/users", tags=["Users"])
 
 
 @router.post("/register", response_model=UserResponse)
 def register(user: UserCreate, db: Session = Depends(get_db)):
     return create_user(db, user)
+
+@router.post("/login")
+def login(user: UserLogin, db: Session = Depends(get_db)):
+    db_user = authenticate_user(db, user.email, user.password)
+
+    if not db_user:
+        raise HTTPException(status_code=401, detail="Invalid email or password")
+
+    return {
+        "message": "Login successful",
+        "user": {
+            "id": db_user.id,
+            "username": db_user.username,
+            "email": db_user.email
+        }
+    }
--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.models.user import User
 from app.schemas.user import UserCreate
 from app.services.auth import hash_password
+from app.services.auth import verify_password
 
 
 def create_user(db: Session, user: UserCreate):
@@ -19,3 +20,16 @@ def create_user(db: Session, user: UserCreate):
     db.refresh(new_user)
 
     return new_user
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user:
+        return None
+
+    if not verify_password(password, user.password):
+        return None
+
+    return user 
+
